aaa95c70b921b58fd454f815f9a24109cbf7ec65
ianet <ianet@users.noreply.github.com>
2021-12-11 10:45:10 -1000

    init

---
+++ src/IMisc.jl
@@ -0,0 +1,15 @@
+module IMisc
+
+"""
+    Void
+
+A type with no fields that is the type of void. Anything can be converted to type Void
+"""
+struct Void end
+
+const void = Void()
+Void(x::Any) = void
+Base.convert(::Type{Void}, x::Any) = void
+Base.convert(::Type{Void}, v::Void) = void
+
+end # module
