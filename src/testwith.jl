d7d71fbb83f7a75da5bef2f7dd952937ed8bc8b1
ianet <ianet@users.noreply.github.com>
2022-01-09 18:41:44 -0800

    currying equiv

--- src/testwith.jl
+++ src/testwith.jl
@@ -5,16 +5,19 @@ struct Win
     value::Int
 end
 
-function move(win::Win, x::Int, y::Int)
+function move(win::Win, x::Int, y::Int)::Win
     println("Move($(win.value)) $x $y")
+    return win
 end
 
-function size(win::Win, w::Int, h::Int)
+function size(win::Win, w::Int, h::Int)::Win
     println("Size($(win.value)) $w $h")
+    return win
 end
 
-function setVisible(win::Win, vis::Bool)
+function setVisible(win::Win, vis::Bool)::Win
     println("Visible($(win.value)) $vis")
+    return win
 end
 
 win = Win(42)
@@ -28,3 +31,13 @@ win = Win(42)
     size(50, 50) 
     setVisible(false)
 end
+
+move(x::Int, y::Int) = (win) -> move(win, x, y)
+size(x::Int, y::Int) = (win) -> size(win, x, y)
+setVisible(vis::Bool) = (win) -> setVisible(win, vis)
+
+win |> 
+    move(11, 11) |> 
+    size(22, 22) |> 
+    setVisible(true)
+
