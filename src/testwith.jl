b7addb4fb2ba0980163684a72e8dacb4c964303c
ianet <ianet@users.noreply.github.com>
2022-04-03 13:29:24 -0700

    more tests

--- src/testwith.jl
+++ src/testwith.jl
@@ -1,22 +1,34 @@
 include("IMisc.jl")
 using .IMisc
 
-struct Win
-    value::Int
+mutable struct Win
+    id::Int
+    x::Int
+    y::Int
+    w::Int
+    h::Int
+    visible::Bool
 end
 
+Win(id::Int)::Win = Win(id, 0, 0, 0, 0, false)
+
 function move(win::Win, x::Int, y::Int)::Win
-    println("Move($(win.value)) $x $y")
+    win.x = x
+    win.y = y
+    # dump(win)
     return win
 end
 
 function size(win::Win, w::Int, h::Int)::Win
-    println("Size($(win.value)) $w $h")
+    win.w = w
+    win.h = h
+    # dump(win)
     return win
 end
 
 function setVisible(win::Win, vis::Bool)::Win
-    println("Visible($(win.value)) $vis")
+    win.visible = vis
+    # dump(win)
     return win
 end
 
@@ -26,18 +38,25 @@ win = Win(42)
 @with win move(20, 20) 
 @with win move(20, 20) size(30, 30) setVisible(false)
 
-@with win begin
-    move(40, 40)
-    size(50, 50) 
-    setVisible(false)
+function testWithBegin()
+    @with win begin
+        move(40, 40)
+        size(50, 50) 
+        setVisible(false)
+    end
 end
 
+testWithBegin()
+
 move(x, y) = (win) -> move(win, x, y)
-size(x, y) = (win) -> size(win, x, y)
+size(w, h) = (win) -> size(win, w, h)
 setVisible(vis) = (win) -> setVisible(win, vis)
 
-win |> 
-    move(11, 11) |> 
-    size(22, 22) |> 
-    setVisible(true)
+function testPipe()
+    win |> 
+        move(11, 12) |> 
+        size(22, 23) |> 
+        setVisible(true)
+end
 
+testPipe()
