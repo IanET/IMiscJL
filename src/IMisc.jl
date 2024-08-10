42fbb33362abe11b7b732349bf2d9151feb9fac8
Ian Ellison-Taylor <IanET@users.noreply.github.com>
2024-08-10 12:09:30 -0700

    Replace retrefs with inlineref (#4)

--- src/IMisc.jl
+++ src/IMisc.jl
@@ -22,29 +22,19 @@ A type that is either T or Nothing
 """
 const Maybe{T} = Union{T, Nothing}
 
-@generated function retrefs_impl(f::Function, args...)
-    refargexps = Expr[]
-    for i in eachindex(args)
-        if args[i] <: Base.RefValue
-            push!(refargexps, :(args[$i]))
-        end
-    end
-    return Expr(:tuple, refargexps..., :(f(args...)))
-end
+
 
 """
-    @retrefs 
+    @inlineref 
 
-Macro to capture ref arguments in a call expression and return their values in a tuple along with the function return value
-Effectively emulating [out] params common in ccall without needing to manually pre-create the references
-
+Macro to extract ref expressions in a call expression as assignments in the local context.
 eg
 
 Given:
 
 ```
-    function foo(r::Ref{Int})::Bool
-        r[] += 1
+    function foo(a, b, out::Ref{Int})::Bool
+        out[] = a + b
         return true
     end
 ```
@@ -53,33 +43,33 @@ Given:
 Instead of:
 
 ```
-    function wrapfoo()
-        r = Ref(Int(0))
-        retval = foo(r)
-        return r, retval
-    end
+    rout = Ref{Int}(0)
+    res = foo(2, 3, rout)
 ```
 
 Simply write:
 
 ```
-    result = @retrefs foo(Int(0))
+    res = @inlineref foo(2, 3, rout = Ref{Int}(0))
 ```
 
 """
-macro retrefs(fex::Expr)
-    @assert fex.head == :call "Expression must be a function call"
-    func = fex.args[1]
-    args = fex.args[2:end]
-    return esc(
-        quote 
-            let 
-                local _tres = $retrefs_impl($func, $(args...))
-                local _vals = Tuple([r[] for r in _tres[begin:end-1]])
-                (_vals..., _tres[end])
-            end
+# Extract inline ref assignments
+macro inlineref(inexpr::Expr)
+    @assert inexpr.head == :call "Expression must be a function call"
+    outexpr = Expr[]
+    params = []
+    for arg in inexpr.args[2:end]
+        if typeof(arg) <: Expr && arg.head == :kw
+            lhs = arg.args[1]
+            push!(params, lhs)
+            push!(outexpr, Expr(Symbol('='), lhs, arg.args[2]))
+        else
+            push!(params, arg)
         end
-    )
+    end
+    push!(outexpr, Expr(:call, inexpr.args[1], params...))
+    return esc(Expr(:block, outexpr...))
 end
 
 end # module
