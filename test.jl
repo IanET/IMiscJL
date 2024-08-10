50aadbd795de6b3a0785a29adbac8a84b61c5c7f
Ian Ellison-Taylor <IanET@users.noreply.github.com>
2024-08-10 11:59:26 -0700

    ...

--- test.jl
+++ test.jl
@@ -13,6 +13,14 @@ function f2(a::Int, b::Int, c::Ref{Int}, d::Ref{Int})::Bool
     return false
 end
 
+function f3(a::Int, b::Int)
+    return a + b
+end
+
+function f4()
+    return 42
+end
+
 # Extract inline ref assignments
 macro inlineref(inexpr::Expr)
     @assert inexpr.head == :call "Expression must be a function call"
@@ -35,8 +43,12 @@ function test()
     res = @inlineref f1(2, 3, out = Ref{Int}(0))
     @info res out[]
 
-    res = @inlineref f2(2, 3, out1 = Ref{Int}(0), out2 = Ref{Int}(0))
+    res = @inlineref f2(2, 3, out1 = Ref{Int}(1), out2 = Ref{Int}(2))
     @info res out1[] out2[]
+
+    @info @inlineref f3(1, 2)
+
+    @info @inlineref f4()
 
 end
 
